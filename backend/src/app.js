1aea6cf8a518c03492bc9725c583bac79f875eac
Oreen <car.azulgarcia@gmail.com>
2026-05-25 09:32:16 -0700

    : static files frontend + env vars FRONTEND_URL y JWT_SECRET

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import { router as authRegisterRouter } from "./modules/auth/register/register.routes.js";
 import logoutRoutes from "./modules/auth/logout/logout.routes.js";
 import { router as authLoginRouter } from "./modules/auth/login/login.routes.js";
 import { router as productsRouter } from "./modules/products/products.routes.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../../frontend")));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
