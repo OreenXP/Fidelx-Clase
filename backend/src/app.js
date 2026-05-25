17be4de9a7519fd1af38341f692a0370f1c5450c
Tadeo Sandoval <tadeolmd@gmail.com>
2026-05-25 08:08:37 -0700

    feat(products): implement CRUD operations and routes for products management

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { router as authRegisterRouter } from "./modules/auth/register/register.routes.js";
 import logoutRoutes from "./modules/auth/logout/logout.routes.js";
 import { router as authLoginRouter } from "./modules/auth/login/login.routes.js";
+import { router as productsRouter } from "./modules/products/products.routes.js";
 const app = express();
 
 app.use(express.json());
@@ -13,4 +14,5 @@ app.get("/health", (req, res) => {
 app.use("/auth", authRegisterRouter);
 app.use("/api/logout", logoutRoutes);
 app.use("/login", authLoginRouter);
+app.use("/products", productsRouter);
 export default app;
